162bbe375d696ecbb2eb6b10632e308dd0875b45
808dyn <surya080201@gmail.com>
2023-08-12 23:19:17 +0700

    first commit

---
+++ app/src/main/java/com/rezzarn/uascatatan/Profile.java
@@ -0,0 +1,18 @@
+package com.rezzarn.uascatatan;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+//  NIM     : 10519008
+//  Nama    : Daniel Dayan
+//  Kelas   : IF1
+
+public class Profile extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_profile);
+    }
+}
---
+++ app/src/main/java/com/rezzarn/uascatatan/TentangAplikasi.java
@@ -0,0 +1,18 @@
+package com.rezzarn.uascatatan;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+//  NIM     : 10519008
+//  Nama    : Daniel Dayan
+//  Kelas   : IF1
+
+public class TentangAplikasi extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_tentang_aplikasi);
+    }
+}
